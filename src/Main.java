98fd4d8136df53abc6d934bd0f7f8fb528c8db92
Alyssa Cuignet <alyssacuignet@Alyssas-MacBook-Air.local>
2022-11-05 21:43:59 +0000

    Whole number primitive data types java practice

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value =" + myMinIntValue);
+        System.out.println("Integer Maximum Value =" + myMaxIntValue);
+        System.out.println("Busted MAX Value =" + (myMaxIntValue + 1) ); //overflow
+        System.out.println("Busted MIN Value =" + (myMinIntValue - 1) ); //underflow
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value =" + myMinByteValue);
+        System.out.println("Byte Maximum Value =" + myMaxByteValue);
+
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value =" + myMinShortValue);
+        System.out.println("Short Maximum Value =" + myMaxShortValue);
+
+        long myLongValue = 100L; //L added after value to indicate that it is a long value.
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value =" + myMinLongValue);
+        System.out.println("Long Maximum Value =" + myMaxLongValue);
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte)(myMinByteValue / 2);
+
+        short myNewShortValue = (short)(myMinByteValue / 2);
+
+    }
+}
